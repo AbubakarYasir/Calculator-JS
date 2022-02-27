92a2538881f695e4ece3d801671e440e1ae229c6
AbubakarYasir <abubakaryasir569@gmail.com>
2022-02-27 23:29:25 +0500

    Completed this simple Calculator

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const port = 3000;
+
 const app = express();
-const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, '/static/index.html'));
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/index.html", function (req, res) {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+
+    let result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
 });
 
 app.listen(port, () => {
